f9590cf076a5552e3424f573bd69436d7bd09c1e
Jacob Van Schenck <jacob.vanschenck@gmail.com>
2025-01-10 11:27:29 -0600

    Update config

--- backend/hardhat.config.ts
+++ backend/hardhat.config.ts
@@ -1,11 +1,14 @@
-import type { HardhatUserConfig } from "hardhat/config";
+import { type HardhatUserConfig, vars } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox-viem";
+
+const PRIVATE_KEY = vars.get("PRIVATE_KEY");
 
 const config: HardhatUserConfig = {
 	solidity: "0.8.28",
 	networks: {
 		base: {
 			url: "https://mainnet.base.org",
+			accounts: [PRIVATE_KEY],
 		},
 		hardhat: {
 			forking: {
